da4d934978ea110f7fd1921dfada88d6bfb3d508
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-03 01:13:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
-  if phrase = "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = "Hi Nana, how are you?"
+  elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = "Hi!"
+  elsif phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = "WHAT DID YOU EAT TODAY?"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938"
   end
 end
