f70f69c5e3b6e1626b0731fd08360a490145973d
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-03 01:08:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif "i love you grandma"
+  elsif phrase = "i love you grandma"
     "HUH?! SPEAK UP, SONNY!"
-
+  else = ""
+    
+  end
 end
 
 
