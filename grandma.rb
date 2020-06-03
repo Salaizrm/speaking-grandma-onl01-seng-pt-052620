0e98e1a63278f7a7db852f655c257cb0ee6c6c30
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-03 01:08:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = "i love you grandma"
+  elsif phrase = "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
   else phrase = ""
     "HUH?! SPEAK UP, SONNY!"
