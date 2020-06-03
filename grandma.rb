b3c7c0d06a3838988710a9fc069a2fa6ecea7a7d
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-03 01:13:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
-    return "NO, NOT SINCE 1938"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
